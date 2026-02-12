77d00c50e3416c4cc4c662387c496fab0b889638
yasohatemaly-maker <yasohatemaly@gmail.com>
2026-02-12 15:54:29 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Future interactivity: For now, just a button click example
+const btn = document.querySelector('.hero .btn');
+btn.addEventListener('click', () => {
+    alert('Welcome to TrueFunded! Start your trading journey.');
+});
